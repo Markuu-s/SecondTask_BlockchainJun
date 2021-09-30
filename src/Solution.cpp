0756a2a18cf6f3dbb8c052049b6c1f927706d9eb
Markuu-s <qwert11223381@gmail.com>
2021-09-30 23:44:23 +0300

    [dev] Realize function isExist.

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -4,3 +4,10 @@ std::map<std::string, Solution::Data> Solution::getInformation()
 {
     return table;
 }
+
+void Solution::isExist(const Data &data)
+{
+    table[data.name].requestsHour += data.requestsHour;
+    table[data.name].countHour += data.countHour;
+    table[data.name].additionalInf.push_back(data.additionalInf[0]);
+}
