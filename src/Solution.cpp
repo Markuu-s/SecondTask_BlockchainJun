a3f80b759201b00347af936dd49e073148cb5081
Markuu-s <qwert11223381@gmail.com>
2021-09-30 23:45:42 +0300

    [dev] Realize function solve.

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -1,4 +1,5 @@
 #include <Soludtion.hpp>
+#include <iostream>
 
 std::map<std::string, Solution::Data> Solution::getInformation()
 {
@@ -10,4 +11,22 @@ void Solution::isExist(const Data &data)
     table[data.name].requestsHour += data.requestsHour;
     table[data.name].countHour += data.countHour;
     table[data.name].additionalInf.push_back(data.additionalInf[0]);
+}
+
+void Solution::solve()
+{
+    freopen("../txtFiles/input1.txt", "r", stdin);
+    
+    Data data;
+    data.additionalInf.resize(1);
+    std::cin >> data.name >> data.requestsHour >> data.countHour >> data.additionalInf[0];
+
+    if (table.find(data.name) == table.end())
+    {
+        table[data.name] = data;
+    }
+    else
+    {
+        isExist(data);
+    }
 }
